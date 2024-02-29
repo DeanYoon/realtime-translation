d8498f7b6dc2be019849b94c989e50402125c254
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2024-02-29 20:55:33 +0900

    feat: navigator input 자동 사라짐

--- components/navigator.tsx
+++ components/navigator.tsx
@@ -1,7 +1,7 @@
 import { useDBStore } from "@/lib/store";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 
 export interface IChannel {
@@ -18,7 +18,9 @@ export default function Nav() {
   const getChannels = useDBStore((state) => state.getChannels);
   const [isAdding, setIsAdding] = useState(false);
 
+  const inputRef = useRef<HTMLInputElement>(null);
   const { register, handleSubmit } = useForm();
+  const { ref } = register("");
 
   const onSubmit = async (data: any) => {
     const { newChannelName } = data;
@@ -31,9 +33,25 @@ export default function Nav() {
     setChannel(id);
   };
 
+  //input 밖 클릭 시 input 사라지게
+  const handleClickOutside = (event: any) => {
+    if (inputRef.current && !inputRef.current.contains(event.target)) {
+      // Click occurred outside the input element
+      setIsAdding(false);
+    }
+  };
+
   useEffect(() => {
     getChannels();
+
+    //input 밖 클릭 시 input 사라지게
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
   }, []);
+
   return (
     <div className="border max-w-32 w-full p-2">
       {channels.map((channel) => (
@@ -49,14 +67,18 @@ export default function Nav() {
       ))}
       {isAdding ? (
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            className="w-full bg-transparent border"
-            type="text"
-            {...register("newChannelName", { required: true, value: "" })}
-          />
+          <div ref={inputRef} onClick={handleClickOutside}>
+            {/* input 밖 클릭 시 input 사라지게 */}
+
+            <input
+              className="w-full bg-transparent border"
+              type="text"
+              {...register("newChannelName", { required: true, value: "" })}
+            />
+          </div>
         </form>
       ) : (
-        <div onClick={() => setIsAdding(true)}>
+        <div onClick={() => setIsAdding(true)} className="cursor-pointer">
           <FontAwesomeIcon icon={faPlus} size="xl" />
         </div>
       )}
